6c3e8b5db88c45a72e51fa8282fbff5132637747
sabrina <hoekehouse@gmail.com>
2020-02-18 16:58:06 -0800

    Work modal full functioning for desktop

--- js/work-behave.js
+++ js/work-behave.js
@@ -1,77 +1,51 @@
 $(document).ready(function(){
 
-    $(".work1").mouseenter(function() {
+    $(".close").click(function() {
+        $(".modal-container").css("display", "none");
+        $(".dark-bg").hide();
+    });
+
+    $(".work1").click(function() {
+        $(".work1-modal").css("display", "flex");
         $(".dark-bg").fadeIn(400);
-        $(".work1-modal").fadeIn(400);
-        
-    });
-    /*
-    $(".work1").mouseleave(function() {
-        $(".work1-modal").hide();
-        $(this).css("z-index", "1");
-    });*/
-
-    $(".work2").mouseenter(function() {
-        $(".work2-modal").show();
-        $(this).css("z-index", "5");
-    });
-    $(".work2").mouseleave(function() {
-        $(".work2-modal").hide();
-        $(this).css("z-index", "1");
+        $(".work1-modal").fadeIn(400);   
     });
 
-    $(".work3").mouseenter(function() {
-        $(".work3-modal").show();
-        $(this).css("z-index", "5");
+    $(".work2").click(function() {
+        $(".work2-modal").css("display", "flex");
+        $(".dark-bg").fadeIn(400);
+        $(".work2-modal").fadeIn(400);
     });
-    $(".work3").mouseleave(function() {
-        $(".work3-modal").hide();
-        $(this).css("z-index", "1");
+    $(".work3").click(function() {
+        $(".work3-modal").css("display", "flex");
+        $(".dark-bg").fadeIn(400);
+        $(".work3-modal").fadeIn(400);
     });
-
-    $(".work4").mouseenter(function() {
-        $(".work4-modal").show();
-        $(this).css("z-index", "5");
-    });
-    $(".work4").mouseleave(function() {
-        $(".work4-modal").hide();
-        $(this).css("z-index", "1");
+    $(".work4").click(function() {
+        $(".work4-modal").css("display", "flex");
+        $(".dark-bg").fadeIn(400);
+        $(".work4-modal").fadeIn(400);
     });
 
-    $(".work5").mouseenter(function() {
-        $(".work5-modal").show();
-        $(this).css("z-index", "5");
+    $(".work5").click(function() {
+        $(".work5-modal").css("display", "flex");
+        $(".dark-bg").fadeIn(400);
+        $(".work5-modal").fadeIn(400);
     });
-    $(".work5").mouseleave(function() {
-        $(".work5-modal").hide();
-        $(this).css("z-index", "1");
-    });
-
-    $(".work6").mouseenter(function() {
-        $(".work6-modal").show();
-        $(this).css("z-index", "5");
-    });
-    $(".work6").mouseleave(function() {
-        $(".work6-modal").hide();
-        $(this).css("z-index", "1");
+     $(".work6").click(function() {
+        $(".work6-modal").css("display", "flex");
+        $(".dark-bg").fadeIn(400);
+        $(".work6-modal").fadeIn(400);
     });
 
-    $(".work7").mouseenter(function() {
-        $(".work7-modal").show();
-        $(this).css("z-index", "5");
+    $(".work7").click(function() {
+        $(".work7-modal").css("display", "flex");
+        $(".dark-bg").fadeIn(400);
+        $(".work7-modal").fadeIn(400);
     });
-    $(".work7").mouseleave(function() {
-        $(".work7-modal").hide();
-        $(this).css("z-index", "1");
+    $(".work8").click(function() {
+        $(".work8-modal").css("display", "flex");
+        $(".dark-bg").fadeIn(400);
+        $(".work8-modal").fadeIn(400);
     });
-
-    $(".work8").mouseenter(function() {
-        $(".work8-modal").show();
-        $(this).css("z-index", "5");
-    });
-    $(".work8").mouseleave(function() {
-        $(".work8-modal").hide();
-        $(this).css("z-index", "1");
-    });
-
 });
